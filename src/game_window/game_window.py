48e9ed351191113750a432262ebea3de1a47827e
atari-monk <atari.monk1@gmail.com>
2025-06-14 14:41:44 +0200

    window/integrate_menu

--- src/game_window/game_window.py
+++ src/game_window/game_window.py
@@ -1,7 +1,8 @@
-# File: C:\atari-monk\code\race-track-game\src\game_window\game_window.py
 from typing import Final
 from arcade import Window, set_background_color, color, run
+from src.main_menu.menu_navigation import NavigationController
 from src.main_menu.main_menu_view import MainMenuView
+from src.main_menu.settings_menu_view import SettingsMenuView
 
 class GameWindow(Window):
     WIDTH: Final[int] = 1024
@@ -12,9 +13,15 @@ class GameWindow(Window):
         super().__init__(self.WIDTH, self.HEIGHT, self.TITLE, resizable=True)
         self.set_fullscreen(False)
         set_background_color(color.BLACK)
+        controller = NavigationController(self, self._main_menu_factory, self._settings_menu_factory)
+        controller.go_to_main_menu()
+
+    def _main_menu_factory(self, controller: NavigationController) -> MainMenuView:
+        return MainMenuView(controller)
+
+    def _settings_menu_factory(self, controller: NavigationController) -> SettingsMenuView:
+        return SettingsMenuView(controller)
 
 if __name__ == "__main__":
     window = GameWindow()
-    window.show_view(MainMenuView())
     run()
-
